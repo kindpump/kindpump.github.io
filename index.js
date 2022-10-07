a56e46adbf9b1a3f73582165ac0b03d3b682fe26
kindpump <115276781+kindpump@users.noreply.github.com>
2022-10-07 14:32:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-// Core, Team, and Official extensions can `require` VM code:
-const ArgumentType = require('../../extension-support/argument-type');
-const BlockType = require('../../extension-support/block-type');
-
-class SomeBlocks {
-    // ...
-    getInfo () {
+class ScratchDillyd {
+    constructor() {
+    }
+    
+    getInfo() {
         return {
-            id: 'someBlocks',
-            name: 'Some Blocks',
-            blocks: [
-                {
-                    opcode: 'myReporter',
-                    blockType: BlockType.REPORTER,
-                    text: 'letter [LETTER_NUM] of [TEXT]',
-                    arguments: {
-                        LETTER_NUM: {
-                            type: ArgumentType.STRING,
-                            defaultValue: '1'
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [
+                        {
+                            "opcode": "notifyMe",
+                            "blockType": "COMMAND",
+                            "text": "Notify user: [notification]",
+                            "arguments": {
+                                "NOTIF": {
+                                    "type": "string",
+                                    "defaultValue": "Hello World"
+                                },
+                            }
                         },
-                        TEXT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'text'
-                        }
-                    }
-                }
-            ]
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
+                            }
+                        },
+                ],
         };
     }
-    // ...
+    
+    notifyMe({NOTIF}) {
+        return alert(NOTIF).then(response => response.text())
+    }
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
+    }
 }
+
+Scratch.extensions.register(new ScratchDillyd())
