05edf1a569b411273f150e37becb468731fc583b
kindpump <115276781+kindpump@users.noreply.github.com>
2022-10-09 17:16:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
-const ArgumentType = require('../extension-support/argument-type');
-const BlockType = require('../extension-support/block-type');
-const dispatch = require('../dispatch/worker-dispatch');
-const TargetType = require('../extension-support/target-type');
+// We use class syntax to define our extension object
+// This isn't actually necessary, but it tends to look the best
 
-class ExtensionWorker {
-    constructor () {
-        this.nextExtensionId = 0;
+class MyExtension {
+  /**
+   * Scratch will call this method *once* when the extension loads.
+   * This method's job is to tell Scratch things like the extension's ID, name, and what blocks it supports.
+   */
+  getInfo() {
+    return {
+      // `id` is the internal ID of the extension
+      // It should never change!
+      // If you choose to make an actual extension, please change this to something else.
+      // Only the characters a-z and 0-9 can be used. No spaces or special characters.
+      id: 'gpsextension',
 
-        this.initialRegistrations = [];
+      // `name` is what the user sees in the toolbox
+      // It can be changed without breaking projects.
+      name: 'GPS Extension',
 
-        dispatch.waitForConnection.then(() => {
-            dispatch.call('extensions', 'allocateWorker').then(x => {
-                const [id, extension] = x;
-                this.workerId = id;
-
-                try {
-                    importScripts(extension);
-
-                    const initialRegistrations = this.initialRegistrations;
-                    this.initialRegistrations = null;
-
-                    Promise.all(initialRegistrations).then(() => dispatch.call('extensions', 'onWorkerInit', id));
-                } catch (e) {
-                    dispatch.call('extensions', 'onWorkerInit', id, e);
-                }
-            });
-        });
-
-        this.extensions = [];
-    }
-
-    register (extensionObject) {
-        const extensionId = this.nextExtensionId++;
-        this.extensions.push(extensionObject);
-        const serviceName = `extension.${this.workerId}.${extensionId}`;
-        const promise = dispatch.setService(serviceName, extensionObject)
-            .then(() => dispatch.call('extensions', 'registerExtensionService', serviceName));
-        if (this.initialRegistrations) {
-            this.initialRegistrations.push(promise);
+      blocks: [
+        {
+          // `opcode` is the internal ID of the block
+          // It should never change!
+          // It corresponds to the class method with the same name.
+          opcode: 'getgps',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Hello, world!'
         }
-        return promise;
-    }
+      ]
+    };
+  }
+
+  /**
+   * Corresponds to `opcode: 'hello'` above
+   */
+  getgps() {
+    // You can just return a value: any string, boolean, or number will work.
+    // If you have to perform an asynchronous action like a request, just return a Promise.
+    // The block will wait until the Promise resolves and return the resolved value.
+    return 'Hello, world!';
+  }
 }
 
-global.Scratch = global.Scratch || {};
-global.Scratch.ArgumentType = ArgumentType;
-global.Scratch.BlockType = BlockType;
-global.Scratch.TargetType = TargetType;
-
-/**
- * Expose only specific parts of the worker to extensions.
- */
-const extensionWorker = new ExtensionWorker();
-global.Scratch.extensions = {
-    register: extensionWorker.register.bind(extensionWorker)
-};
+// Call Scratch.extensions.register to register your extension
+// Make sure to register each extension exactly once
+Scratch.extensions.register(new MyExtension());
