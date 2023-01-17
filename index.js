38a9a3b3127ac21d2afe019109841d7893dad4c1
kindpump <115276781+kindpump@users.noreply.github.com>
2023-01-17 01:42:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,21 @@ class ScratchChatbot {
 
     getResponse({message}) {
         // Logic to send message to chatbot API and receive the chatbot's response
-        return fetch('https://your-chatbot-api.com/message', {
+        const session_id = '123456789';
+        const project_id = 'your_project_id';
+        const access_token = 'your_access_token';
+        const url = 'https://dialogflow.googleapis.com/v2/projects/'+ project_id + '/agent/sessions/' + session_id + ':detectIntent';
+        return fetch(url, {
           method: 'POST',
-          body: JSON.stringify({message: message}),
-          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({queryInput: {text: {text: message, languageCode: "en"}}}),
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+          },
         })
         .then(res => res.json())
         .then(response => {
-           return response.chatbot_response;
+           return response.queryResult.fulfillmentText;
         })
         .catch(error => {
            return error;
