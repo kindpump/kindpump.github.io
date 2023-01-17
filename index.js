062aee03c87e305d79ea9658190ca513f9924acd
kindpump <115276781+kindpump@users.noreply.github.com>
2023-01-17 01:04:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,43 @@ class ScratchFetch {
                                 },
                             }
                         },
-                    
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
+                            }
+                        },
                 ],
         };
     }
     
     fetchURL({url}) {
-       function showLocation(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-        
-        return  navigator.geolocation.getCurrentPosition(url)
-            .then(response => response.text())
- 
+        return fetch(url).then(response => response.text())
+    }
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
     }
 }
 
