9781614862790e350b52bc770e7328decfd4e0f3
kindpump <115276781+kindpump@users.noreply.github.com>
2023-01-17 01:45:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-class ScratchChatbot {
+class ScratchGeoLocation {
     constructor() {
     }
 
     getInfo() {
         return {
-            "id": "Chatbot",
-            "name": "Chatbot",
+            "id": "GeoLocation",
+            "name": "GeoLocation",
             "blocks": [
                         {
-                            "opcode": "getResponse",
+                            "opcode": "getCoordinates",
                             "blockType": "reporter",
-                            "text": "chatbot says [message]",
-                            "arguments": {
-                                "message": {
-                                    "type": "string",
-                                    "defaultValue": ""
-                                }
-                            }
+                            "text": "My Coordinates",
                         },
                 ],
         };
     }
 
-    getResponse({message}) {
-        // Logic to send message to chatbot API and receive the chatbot's response
-        const session_id = '123456789';
-        const project_id = 'your_project_id';
-        const access_token = 'your_access_token';
-        const url = 'https://dialogflow.googleapis.com/v2/projects/'+ project_id + '/agent/sessions/' + session_id + ':detectIntent';
-        return fetch(url, {
-          method: 'POST',
-          body: JSON.stringify({queryInput: {text: {text: message, languageCode: "en"}}}),
-          headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + access_token
-          },
-        })
-        .then(res => res.json())
-        .then(response => {
-           return response.queryResult.fulfillmentText;
-        })
-        .catch(error => {
-           return error;
+    getCoordinates() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const coordinates = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    }
+                    resolve(coordinates);
+                },
+                error => reject(error)
+            );
         });
     }
 }
 
-Scratch.extensions.register(new ScratchChatbot());
+Scratch.extensions.register(new ScratchGeoLocation());
