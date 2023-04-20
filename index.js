aa3c60918d725973157c6ec3a34a2074b6ab8c2b
kindpump <115276781+kindpump@users.noreply.github.com>
2023-04-19 21:18:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,59 @@
-class ScratchSimpleGamepad {
-    constructor(runtime) {
-        this.runtime = runtime
-        this.currentMSecs = -1
-        this.previousButtons = []
-        this.currentButtons = []
-    }
-    
-    getInfo() {
-        return {
-            "id": "SimpleGamepad",
-            "name": "SimpleGamepad",
-            "blocks": [{
-                        "opcode": "buttonPressedReleased",
-                        "blockType": "hat",
-                        "text": "button [b] [eventType]",
-                        "arguments": {
-                            "b": {
-                                "type": "number",
-                                "defaultValue": "0"
-                            },
-                            "eventType": {
-                                "type": "number",
-                                "defaultValue": "1",
-                                "menu": "pressReleaseMenu"
-                            },
-                        },
-                    },
-            ],
-            "menus": {
-                "pressReleaseMenu": [{text:"press",value:1}, {text:"release",value:0}],
-            }            
-        };
-    }
-    
-    update() {
-        if (this.runtime.currentMSecs == this.currentMSecs) 
-            return // not a new polling cycle
-        this.currentMSecs = this.runtime.currentMSecs
-        var gamepads = navigator.getGamepads()
-        if (gamepads == null || gamepads.length == 0 || gamepads[0] == null) {
-            // different number of buttons, so new gamepad
-            this.previousButtons = []
-            this.currentButtons = []
-            return
-        }
-        var gamepad = gamepads[0]
-        if (gamepad.buttons.length != this.previousButtons.length) {
-            this.previousButtons = []
-            for (var i = 0; i < gamepad.buttons.length; i++) 
-                this.previousButtons.push(false)
-        }
-        else {
-            this.previousButtons = this.currentButtons
-        }
-        this.currentButtons = []
-        for (var i = 0; i < gamepad.buttons.length; i++) 
-            this.currentButtons.push(gamepad.buttons[i].pressed)
-    }
-    
-    buttonPressedReleased({b,eventType}) {
-        this.update()
-        if (b < this.currentButtons.length) {
-            if (eventType == 1) { // note: this will be a string, so better to compare it to 1 than to treat it as a Boolean
-                if (this.currentButtons[b] && ! this.previousButtons[b]) {
-                    return true
-                }
+class GPTExtension {
+  constructor(runtime) {
+    this.runtime = runtime;
+    this.text = '';
+    this.apiKey = '';
+  }
+
+  getInfo() {
+    return {
+      id: 'gpt',
+      name: 'GPT-3 Text Completion',
+      blocks: [
+        {
+          opcode: 'completeText',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'complete [TEXT] using GPT-3 with API key [APIKEY]',
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'The quick brown fox'
+            },
+            APIKEY: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'YOUR_API_KEY_HERE'
             }
-            else {
-                if (!this.currentButtons[b] && this.previousButtons[b]) {
-                    return true
-                }
-             }
+          }
         }
-        return false
-    }
+      ]
+    };
+  }
+
+  completeText(args) {
+    const prompt = args.TEXT;
+    const maxTokens = 50;
+    const apiEndpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+    const apiKey = args.APIKEY;
+
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt,
+        max_tokens: maxTokens
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const completedText = data.choices[0].text;
+      const target = this.runtime.getEditingTarget();
+      target.setVariable('completedText', completedText);
+    })
+    .catch(error => console.error(error));
+  }
 }
 
-(function() {
-    var extensionInstance = new ScratchSimpleGamepad(window.vm.extensionManager.runtime)
-    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
-    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
-})()
+Scratch.extensions.register(new GPTExtension());
