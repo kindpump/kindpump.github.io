7fde9b10ac263476386c6e6166988ae04d04f332
kindpump <115276781+kindpump@users.noreply.github.com>
2023-04-20 00:57:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,112 +1,69 @@
-class GPT {
-  constructor() {
-    this.apiKey = ""; // Set the API key to an empty string by default
-  }
-
-  getInfo() {
-    return {
-      "id": "GPT",
-      "name": "GPT",
-      "blocks": [
-        {
-          "opcode": "fetchURL",
-          "blockType": "reporter",
-          "text": "fetch data from [url]",
-          "arguments": {
-            "url": {
-              "type": "string",
-              "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
-            },
-          }
-        },
-        {
-          "opcode": "jsonExtract",
-          "blockType": "reporter",
-          "text": "extract [name] from [data]",
-          "arguments": {
-            "name": {
-              "type": "string",
-              "defaultValue": "temperature"
-            },
-            "data": {
-              "type": "string",
-              "defaultValue": '{"temperature": 12.3}'
-            },
-          }
-        },
-        {
-          "opcode": "getChatResponse",
-          "blockType": "reporter",
-          "text": "get GPT chat response for [prompt] with API key [apiKey]",
-          "arguments": {
-            "prompt": {
-              "type": "string",
-              "defaultValue": "Hello, GPT!"
-            },
-            "apiKey": {
-              "type": "string",
-              "defaultValue": "",
-            },
-          }
-        },
-      ],
-    };
-  }
-
-  setApiKey(apiKey) {
-    this.apiKey = apiKey;
-  }
-
-  async getChatResponse({ prompt, apiKey }) {
-    if (apiKey) {
-      this.apiKey = apiKey;
+class ScratchFetch {
+    constructor() {
     }
-    try {
-      const response = await fetch(`https://api.openai.com/v1/engines/davinci-codex/completions`, {
+    
+    getInfo() {
+        return {
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [
+                        {
+                            "opcode": "fetchURL",
+                            "blockType": "reporter",
+                            "text": "gpt data from [prompt] and GPT key [key]",
+                            "arguments": {
+                                "prompt": {
+                                    "type": "string",
+                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
+                                },
+                                "key": {
+                                    "type": "string",
+                                    "defaultValue": "gptkeygoeshere"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
+                            }
+                        },
+                ],
+        };
+    }
+    
+    fetchURL({prompt,key}) {
+        return fetch(`https://api.openai.com/v1/engines/davinci-codex/completions`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${this.apiKey}`,
-        },
-        body: JSON.stringify({
-          "prompt": `${prompt}`,
-          "max_tokens": 50,
-          "n": 1,
-          "stop": "\n"
-        })
-      });
-      const data = await response.json();
-      return data.choices[0].text.trim();
-    } catch (error) {
-      console.error(error);
-      return "";
+          'Authorization': `Bearer ${key}.then(response => response.text())
     }
-  }
-
-  fetchURL({ url }) {
-    return fetch(url).then(response => response.text())
-  }
-
-  jsonExtract({ name, data }) {
-    try {
-      var parsed = JSON.parse(data)
-      if (name in parsed) {
-        var out = parsed[name]
-        var t = typeof(out)
-        if (t == "string" || t == "number")
-          return out
-        if (t == "boolean")
-          return t ? 1 : 0
-        return JSON.stringify(out)
-      }
-      else {
-        return ""
-      }
-    } catch (error) {
-      console.error(error);
-      return "";
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
     }
-  }
 }
 
-Scratch.extensions.register(new GPT());
+Scratch.extensions.register(new ScratchFetch())
